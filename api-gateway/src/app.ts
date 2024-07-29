5cd73181084150e4137850795b1c1322674de3cc
Navaneeth V <navaneethvinod18@gmail.com>
2024-07-29 22:01:33 +0530

    API Gateway : Dockerized and fully redesigned the config and proxy-middleware and created new authRoute

--- api-gateway/src/app.ts
+++ api-gateway/src/app.ts
@@ -1,26 +1,22 @@
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import cors from 'cors';
-import { createProxyService } from './middleware/proxy-middleware';
-import logger from './core/logger';
+import express from "express";
+import cors from "cors";
+import logger from "./core/logger";
 
 const app = express();
 const port = 3000;
 
-//* Set up the Swagger UI for serving the API documentation
-const swaggerDoc = YAML.load('../api-gateway/infra/api-doc/swagger.yaml');
+import authRouter from "./middleware/auth-route";
 
 app.use(cors());
 
-//* Proxy route
-app.use('/api/auth', createProxyService('auth'));
+// Proxy routes
 
-//* Serve Swagger API docs
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.get("/", (req, res) => {
+  res.json("Hellooooooo");
+});
 
-//* Start the server
+app.use("/api/auth", authRouter);
+
 app.listen(port, () => {
   logger.info(`API Gateway is running on http://localhost:${port}`);
-  console.log(`Swagger API running on http://localhost:${port}/api-docs`);
 });
